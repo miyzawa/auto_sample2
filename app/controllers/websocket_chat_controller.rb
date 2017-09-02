aca2448fcd3e394bb61573607e0a3a86b1056970
miyzawa <b1504527@planet.kanazawa-it.ac.jp>
2017-09-02 12:51:11 +0900

    コントローラの作成とEventRouterの設定

---
+++ app/controllers/websocket_chat_controller.rb
@@ -0,0 +1,8 @@
+class WebsocketChatController < WebsocketRails::BaseController
+  def message_recieve
+      # クライアントからのメッセージを取得
+      recieve_message = message()
+      # websocket_chatイベントで接続しているクライアントにブロードキャスト
+      broadcast_message(:websocket_chat, recieve_message)
+  end
+end
